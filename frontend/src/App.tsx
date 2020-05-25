bc1876b90d23fb70f71e8cda7d1b83ea599e95ea
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-25 15:43:17 +0900

    それっぽいのが出来た

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,39 +11,6 @@ const Wrapper = styled.div`
   color: #333;
 `
 
-const template = `
-"avatar name name .    .    menu"
-".      body body body body body"
-".      com  like rt   .    ."
-`
-
-const GridLayout = styled.div`
-  display: grid;
-  grid-template: ${template};
-  padding: 0.5em;
-  margin: 1em;
-  grid-gap: 0.2em;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  align-content: center;
-  font-size: 1em;
-`
-
-const Area = styled.div<{ area: string }>`
-  grid-column: ${(props) => props.area};
-  align-self: center;
-`
-
-const Avatar = styled.img`
-  width: 48px;
-`
-
-const Name = styled.div`
-  margin: 0;
-`
-
-const Body = styled.p``
-
 export function App() {
   return (
     <Wrapper>
--- frontend/src/Calendar.tsx
+++ frontend/src/Calendar.tsx
@@ -6,37 +6,35 @@ type Time = {
 }
 const members = ['taro', 'jiro', 'hanako']
 
-const rows = ['time'].concat(members)
+const rows = members
 
+// 時間を30分単位で出力
 const rangeTimes = (start = 6, hours = 24): Time[] => {
   return Array.from({ length: hours * 2 + 1 }, (_, i) => {
     const hr = Math.floor(i / 2) + start
     const min = (i % 2) * 30
     const minSprint = min === 0 ? '00' : min
-
-    // return `${hr}:${minSprint}`;
-    return { hour: hr.toString(), min: minSprint.toString() }
+    const hrSprint = hr < 10 ? `0${hr}` : hr
+    return { hour: hrSprint.toString(), min: minSprint.toString() }
   })
 }
 
-const times = rangeTimes() // 時間を15分単位で排出
+const times = rangeTimes()
 const rowTemplate = ['[t-header]']
   .concat(times.map((time) => `[t-${time.hour}${time.min}]`))
-  .join(' 0.2fr ')
-// rowTemplateは↓こんな感じの文字列に
-// [t-1000] 1fr [t-1015] 1fr [t-1030] 1fr [t-1045] 1fr [t-1100] 1fr [t-1115] 1fr [t-1130] ..
+  .join(' 0.3fr ')
 
 const Grid = styled.div`
   display: grid;
   background: white;
   box-sizing: border-box;
-  grid-template-columns: [time] 0.5fr [taro] 1fr [jiro] 1fr [hanako] 1fr;
-  grid-template-rows: ${rowTemplate};
+  grid-template-rows: [time] 0.5fr [taro] 1fr [jiro] 1fr [hanako] 1fr;
+  grid-template-columns: ${rowTemplate};
 `
 
 const Area = styled.div<{ column: string; rowStart: string; rowEnd?: string }>`
-  grid-column: ${({ column }) => column};
-  grid-row: ${({ rowStart, rowEnd }) => {
+  grid-row: ${({ column }) => column};
+  grid-column: ${({ rowStart, rowEnd }) => {
     if (rowEnd) {
       return `t-${rowStart} / t-${rowEnd}`
     }
@@ -48,36 +46,24 @@ const Area = styled.div<{ column: string; rowStart: string; rowEnd?: string }>`
 const flatten = (item) => item.reduce((a, b) => a.concat(b), [])
 // 全部のエリアにborderを撒き散らす
 
-const Border = styled(Area)<{ color: string }>`
-  border-left: solid 1px #ccc;
+const Border = styled(Area)`
+  border-top: solid 1px #ccc;
   min-height: 0.2rem;
-  border-top: ${({ color = '#ccc' }) => `solid 1px ${color}`};
+  border-left: 1px solid #ccc;
 `
 
 const Borders = () => {
   const elms = rows.map((column) => {
     return times.map((time, i) => (
-      <Border
-        color={time.min == '00' ? '#333' : '#ccc'}
-        column={column}
-        rowStart={`${time.hour}${time.min}`}
-        key={`${column}-${i}`}
-      />
+      <Border column={column} rowStart={`${time.hour}${time.min}`} key={`${column}-${i}`} />
     ))
   })
   return flatten(elms)
 }
 
-// @ts-ignore
-const Times: SFC<{}> = () => {
-  return rangeTimes().map((time, i) => {
-    return (
-      <Area rowStart={`${time.hour}${time.min}`} column={'time'} key={i.toString()}>
-        {time.hour}:{time.min}
-      </Area>
-    )
-  })
-}
+const Time = styled(Area)`
+  font-size: 0.2rem;
+`
 
 const ScheduleBlock = styled(Area)`
   background: #429bf4;
@@ -87,24 +73,11 @@ const ScheduleBlock = styled(Area)`
   padding: 1em;
   margin: 0.1em 0.5em;
   color: #eee;
+  font-size: 0.5rem;
 `
 
-const Schedule: SFC<{ start: string; end: string; name: string }> = ({
-  start,
-  end,
-  name,
-  children,
-}) => {
-  const time = `${start.substr(0, 2)}:${start.substr(2, 4)}`
-  return (
-    <ScheduleBlock rowStart={start} rowEnd={end} column={name}>
-      <div>{time}</div>
-      <div>
-        [{name}
-        ]: {children}
-      </div>
-    </ScheduleBlock>
-  )
+const Schedule: SFC<{ start: string; end: string; name: string }> = ({ start, end, name }) => {
+  return <ScheduleBlock rowStart={start} rowEnd={end} column={name}></ScheduleBlock>
 }
 
 const HeaderCell = styled(Area)`
@@ -117,7 +90,7 @@ const Headers = () => {
       {members.map((member) => {
         return (
           <HeaderCell column={member} rowStart={'header'}>
-            {member}
+            <p>{member}</p>
           </HeaderCell>
         )
       })}
@@ -130,23 +103,12 @@ export const Timetable = () => {
     <Grid>
       <Borders />
       <Headers />
-      <Times />
 
-      <Schedule name="taro" start={'1030'} end={'1145'}>
-        外出
-      </Schedule>
-      <Schedule name="taro" start={'1345'} end={'1600'}>
-        外出
-      </Schedule>
-      <Schedule name="hanako" start={'1130'} end={'1200'}>
-        お昼
-      </Schedule>
-      <Schedule name="jiro" start={'1230'} end={'1400'}>
-        会議
-      </Schedule>
-      <Schedule name="hanako" start={'1230'} end={'1400'}>
-        会議
-      </Schedule>
+      <Schedule name="taro" start={'0830'} end={'0900'}></Schedule>
+      <Schedule name="taro" start={'1300'} end={'2700'}></Schedule>
+      <Schedule name="hanako" start={'1130'} end={'1500'}></Schedule>
+      <Schedule name="jiro" start={'1230'} end={'1600'}></Schedule>
+      <Schedule name="hanako" start={'1030'} end={'1400'}></Schedule>
     </Grid>
   )
 }

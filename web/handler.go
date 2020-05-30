016af04f9e89ae5ac9f8fd22f24d7fb2969ca933
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-30 16:43:04 +0900

    取得ツイート数の制限を追加

--- web/handler.go
+++ web/handler.go
@@ -88,7 +88,7 @@ func (h *Handler) getTweets(accessToken *oauth.AccessToken) ([]*twitter.Tweet, e
 			return nil, err
 		}
 		allTweets = append(allTweets, tweets...)
-		if len(allTweets) > 1000 {
+		if len(allTweets) > 2000 || time.Now().Sub(tweets[len(tweets)-1].Created) > 21*24*time.Hour {
 			break
 		}
 		maxID = allTweets[len(allTweets)-1].ID

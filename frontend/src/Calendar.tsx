54c11ee07915135358d1b78e99c6f09de6ab92aa
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-31 12:44:17 +0900

    電球アイコンを追加

--- frontend/src/Calendar.tsx
+++ frontend/src/Calendar.tsx
@@ -72,7 +72,7 @@ export const Calendar = () => {
     <>
       {rowTemplate.length !== 0 ? (
         <>
-          <p>クリックすることで起床後・就寝前のツイートを見ることができます。</p>
+          <p>💡クリックすることで起床後・就寝前のツイートを見ることができます。</p>
           <Grid rowTemplate={rowTemplate}>
             <Borders dateLabels={dateLabels} timesPerHalfHour={timesPerHalfHour} />
             <DateHeaders dateTexts={dateTexts} />

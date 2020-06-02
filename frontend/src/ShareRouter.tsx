5240a4886c7c0b0c1ad49db9b9e787cff614f6ae
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-03 03:06:12 +0900

    タイトルをセット

--- frontend/src/ShareRouter.tsx
+++ frontend/src/ShareRouter.tsx
@@ -1,25 +1,27 @@
 import { RouteComponentProps } from 'react-router-dom'
 
+const title = '生活習慣の乱れを可視化するやつ'
 const description =
   'ツイートを使って生活習慣の乱れを可視化するWebアプリです。カレンダーUIで直感的に起床・就寝時間の変化を見ることが出来ます。'
 
 export const ShareRouter = ({ match }: RouteComponentProps<{ id: string }>) => {
-  document.title = '生活習慣の乱れを可視化するやつ'
-
+  document.title = title
   const headData = document.head.children
   for (let i = 0; i < headData.length; i++) {
     const nameVal = headData[i].getAttribute('name')
     if (nameVal?.indexOf('description') !== -1) {
       headData[i].setAttribute('content', description)
     }
-    // OGP(twitter)の設定
-    if (nameVal?.indexOf('twitter:image') !== -1) {
+    if (nameVal?.indexOf('og:title') !== -1) {
+      headData[i].setAttribute('content', title)
+    }
+    if (nameVal?.indexOf('og:image') !== -1) {
       headData[i].setAttribute(
         'content',
         `https://storage.googleapis.com/midare-share/${match.params.id}.jpeg`
       )
     }
-    if (nameVal?.indexOf('twitter:description') !== -1) {
+    if (nameVal?.indexOf('og:description') !== -1) {
       headData[i].setAttribute('content', description)
     }
   }

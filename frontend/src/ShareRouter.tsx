92c06ba1933ead0fa1e996e677ef670790f19bc5
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-03 03:22:25 +0900

    コメントを削除

--- frontend/src/ShareRouter.tsx
+++ frontend/src/ShareRouter.tsx
@@ -1,8 +1,6 @@
 import { RouteComponentProps } from 'react-router-dom'
 
 const title = '生活習慣の乱れを可視化するやつ'
-const description =
-  'ツイートを使って生活習慣の乱れを可視化するWebアプリです。カレンダーUIで直感的に起床・就寝時間の変化を見ることが出来ます。'
 
 export const ShareRouter = ({ match }: RouteComponentProps<{ id: string }>) => {
   document.title = title
@@ -13,18 +11,12 @@ export const ShareRouter = ({ match }: RouteComponentProps<{ id: string }>) => {
       continue
     }
 
-    // if (nameVal.indexOf('og:title') !== -1) {
-    //   headData[i].setAttribute('content', title)
-    // }
     if (nameVal.indexOf('og:image') !== -1) {
       headData[i].setAttribute(
         'content',
         `https://storage.googleapis.com/midare-share/${match.params.id}.jpeg`
       )
     }
-    // if (nameVal.indexOf('og:description') !== -1) {
-    //   headData[i].setAttribute('content', description)
-    // }
   }
   return null
 }

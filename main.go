cd11f2d94bf466ee31b1d744d6349f80109663a2
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-13 15:22:02 +0900

    Graceful Shutdownを実装

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/p1ass/midare/web"
-
-	"github.com/p1ass/midare/lib/logging"
 
 	"github.com/p1ass/midare/twitter"
 )
@@ -23,9 +25,30 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	err = router.Run()
-	if err != nil {
-		logger := logging.New()
-		logger.Panic("failed to listen and serve", logging.Error(err))
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
 }

e7b391c7480c251c9f958fad4df38f4929e4ca5a
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-21 12:52:03 +0900

    ファイル名を変更

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
-import { rangeTimes } from '../lib/time'
+import { rangeTimes } from '../lib/Time'
 import {
   convertPeriodsToAwakePeriods,
   getDatesBetweenLatestAndOldest,
--- frontend/src/components/Times.tsx
+++ frontend/src/components/Times.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { rangeTimes } from '../lib/time'
+import { rangeTimes } from '../lib/Time'
 import { Area } from '../atom/Area'
 
 const Hour = styled(Area)<{ generatingImage: boolean }>`

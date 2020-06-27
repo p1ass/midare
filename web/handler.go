aa4ef196fc97699b34eccbfef1db8142b46bd320
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-27 17:11:03 +0900

    リファクタリング

--- web/handler.go
+++ web/handler.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -146,69 +145,6 @@ func (h *Handler) filterByCreated(tweets []*twitter.Tweet) []*twitter.Tweet {
 	return filtered
 }
 
-func (h *Handler) calcAwakePeriods(ts []*twitter.Tweet) []*period {
-	periods := []*period{}
-	var neTweet *twitter.Tweet
-	var okiTweet *twitter.Tweet
-	var lastTweet *twitter.Tweet
-	startIdx := 1
-	for i, t := range ts {
-		if !h.containExcludeWord(t.Text) {
-			neTweet = t
-			okiTweet = t
-			lastTweet = t
-			startIdx = i + 1
-			break
-		}
-	}
-	if lastTweet == nil {
-		return periods
-	}
-
-	for _, t := range ts[startIdx:] {
-		if h.containExcludeWord(t.Text) {
-			continue
-		}
-
-		durationBetweenTweets := lastTweet.Created.Sub(t.Created)
-		if durationBetweenTweets <= awakeThreshold {
-			okiTweet = t
-			lastTweet = t
-			continue
-		}
-
-		if okiTweet != neTweet {
-			periods = append(periods, &period{
-				OkiTime: okiTweet,
-				NeTime:  neTweet,
-			})
-		}
-
-		okiTweet = t
-		neTweet = t
-		lastTweet = t
-	}
-
-	if okiTweet != neTweet {
-		periods = append(periods, &period{
-			OkiTime: okiTweet,
-			NeTime:  neTweet,
-		})
-	}
-
-	return periods
-}
-
-func (h *Handler) containExcludeWord(text string) bool {
-	excludeWords := []string{"ぼくへ 生活習慣乱れてませんか？", "みんなへ 生活習慣乱れてませんか？", "#contributter_report", "のポスト数"}
-	for _, word := range excludeWords {
-		if strings.Contains(text, word) {
-			return true
-		}
-	}
-	return false
-}
-
 func (h *Handler) uploadImage(periods []*period, shareID string) string {
 
 	go h.uploadImageThroughCloudFunctions(shareID, periods)
@@ -235,9 +171,4 @@ func (h *Handler) uploadImageThroughCloudFunctions(uuid string, periods []*perio
 	}
 
 	return nil
-}
-
-type period struct {
-	OkiTime *twitter.Tweet `json:"okiTime"`
-	NeTime  *twitter.Tweet `json:"neTime"`
 }

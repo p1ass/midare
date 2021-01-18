c4bfca470141546697630e62522fd6a5c9d6cc85
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-01-19 00:14:33 +0900

    waitForSelectorのtimeoutを設定

--- ogp_functions/src/index.ts
+++ ogp_functions/src/index.ts
@@ -26,7 +26,7 @@ export async function ogpFunctions(req: Request<any,any,Body>, res: Response) {
     try{
         await page.goto(process.env.OGP_URL || 'http://localhost.local:3000/ogp');
         await page.exposeFunction('getPeriods', ()=> req.body.periods)
-        await page.waitForSelector('.ogp-calendar-flex')
+        await page.waitForSelector('.ogp-calendar-flex',{timeout:5000})
         binary = await page.screenshot({encoding: 'binary'});
         await browser.close();
     }catch(e){

ba3cb522f785dd67b8da7638eaf83378900fbce3
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-02-07 01:16:45 +0900

    card validatorでogpを反映する

--- ogp_functions/src/index.ts
+++ ogp_functions/src/index.ts
@@ -17,7 +17,7 @@ export async function ogpFunctions(req: Request<any, any, Body>, res: Response) 
 
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true
+    headless: process.env.NODE_ENV === 'production'
   })
   const page = (await browser.pages())[0]
   await page.emulateTimezone('Asia/Tokyo')
@@ -29,11 +29,10 @@ export async function ogpFunctions(req: Request<any, any, Body>, res: Response) 
 
   for (let retryCnt = 0; retryCnt < MAX_RETRY_COUNT; retryCnt++) {
     try {
-      await page.goto(process.env.OGP_URL || 'http://localhost.local:3000/ogp')
+      await page.goto(process.env.OGP_URL || 'http://localhost:3000/ogp')
       await page.exposeFunction('getPeriods', () => req.body.periods)
       await page.waitForSelector('.ogp-calendar-flex', { timeout: 5000 })
       binary = await page.screenshot({ encoding: 'binary' })
-      await browser.close()
       break
     } catch (e) {
       if (retryCnt < MAX_RETRY_COUNT) {
@@ -63,6 +62,18 @@ export async function ogpFunctions(req: Request<any, any, Body>, res: Response) 
     console.log('before save')
     await blob.save(binary)
     console.log('after save')
+  } catch (e) {
+    res.status(500).send(e)
+    console.log(e)
+
+    return
+  }
+
+  try {
+    console.log('before update ogp')
+    await updateOGP(page, req.body.uuid)
+    console.log('after update ogp')
+    await browser.close()
     res.status(200).send({})
     return
   } catch (e) {
@@ -70,4 +81,28 @@ export async function ogpFunctions(req: Request<any, any, Body>, res: Response) 
     console.log(e)
     return
   }
+}
+
+const updateOGP = async (page: puppeteer.Page, uuid: string) => {
+  await page.goto('https://cards-dev.twitter.com/validator')
+  await page.waitForSelector(
+    '#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(6) > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-19h5ruw.r-1udh08x.r-1inuy60.r-ou255f.r-m611by > div > input'
+  )
+  await page.type(
+    '#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(6) > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-19h5ruw.r-1udh08x.r-1inuy60.r-ou255f.r-m611by > div > input',
+    process.env.TWITTER_USER || ''
+  )
+  await page.type(
+    '#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(7) > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-19h5ruw.r-1udh08x.r-1inuy60.r-ou255f.r-m611by > div > input',
+    process.env.TWITTER_PASSWORD || ''
+  )
+  await page.click(
+    '#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(8) > div > div'
+  )
+  await page.waitForSelector('#ValidationForm > div > div:nth-child(1) > input.FormControl')
+  await page.type(
+    '#ValidationForm > div > div:nth-child(1) > input.FormControl',
+    `https://midare.p1ass.com/share/${uuid}`
+  )
+  await page.click('#ValidationForm > div > div.Grid-cell.u-sizeFill.u-marginTm > input')
 }

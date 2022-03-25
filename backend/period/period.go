f9b486aec9dd55a3f5d9cf75f854fd8e41db67ee
Naoki Kishi <contact@p1ass.com>
2022-03-26 00:51:59 +0900

    add comment

--- backend/period/period.go
+++ backend/period/period.go
@@ -16,24 +16,16 @@ type Period struct {
 	NeTime  *twitter.Tweet `json:"neTime"`
 }
 
-// CalcAwakePeriods FIX ME テストで挙動を担保してはいるが、ロジックがブラックボックスなのでうまく整理したい
+// CalcAwakePeriods calculates awake periods.
+// IMPORTANT: For Twitter API specification, tweets slice is sorted by created_at desc.
 func CalcAwakePeriods(tweets []*twitter.Tweet) []*Period {
-	periods := []*Period{}
-	var neTweet *twitter.Tweet
-	var okiTweet *twitter.Tweet
-	var lastTweet *twitter.Tweet
-	startIdx := 1
-	for i, t := range tweets {
-		if !t.ContainExcludedWord() {
-			neTweet = t
-			okiTweet = t
-			lastTweet = t
-			startIdx = i + 1
-			break
-		}
-	}
-	if lastTweet == nil {
-		return periods
+	var periods []*Period
+
+	neTweet, lastTweet, startIdx := getMostRecentValidTweet(tweets)
+
+	// ツイートが全く無かった場合は空のスライスを返す
+	if startIdx == 0 {
+		return []*Period{}
 	}
 
 	for _, t := range tweets[startIdx:] {
@@ -42,30 +34,52 @@ func CalcAwakePeriods(tweets []*twitter.Tweet) []*Period {
 		}
 
 		durationBetweenTweets := lastTweet.Created.Sub(t.Created)
-		if durationBetweenTweets <= awakeThreshold {
-			okiTweet = t
-			lastTweet = t
-			continue
+		if durationBetweenTweets > awakeThreshold {
+			// しきい値時間を超えていればその時点でPeriodが確定するので、
+			// lastTweetとneTweetが同じ場合を除きPeriodに追加する
+			//(Periodが切り替わった後のtがしきい値以上間隔が空いている場合に発生)
+			if lastTweet != neTweet {
+				periods = append(periods, &Period{
+					OkiTime: lastTweet,
+					NeTime:  neTweet,
+				})
+			}
+			// Periodが切り替わるので、neTweetを更新する
+			neTweet = t
 		}
 
-		if okiTweet != neTweet {
-			periods = append(periods, &Period{
-				OkiTime: okiTweet,
-				NeTime:  neTweet,
-			})
-		}
-
-		okiTweet = t
-		neTweet = t
 		lastTweet = t
 	}
 
-	if okiTweet != neTweet {
+	// ずっとしきい値以内だった場合はPeriodに追加されることなくループを抜けてしまうので、
+	// ここでPeriodを追加する
+	if lastTweet != neTweet {
 		periods = append(periods, &Period{
-			OkiTime: okiTweet,
+			OkiTime: lastTweet,
 			NeTime:  neTweet,
 		})
 	}
 
+	// 0件の場合は、nullではなく空のJSONを返したいので
+	if len(periods) == 0 {
+		return []*Period{}
+	}
+
 	return periods
+}
+
+// 変数初期化のために、最も最近の除外されないツイートを探している
+func getMostRecentValidTweet(tweets []*twitter.Tweet) (*twitter.Tweet, *twitter.Tweet, int) {
+	var neTweet *twitter.Tweet
+	var lastTweet *twitter.Tweet
+	var startIdx int
+	for i, t := range tweets {
+		if !t.ContainExcludedWord() {
+			neTweet = t
+			lastTweet = t
+			startIdx = i + 1
+			break
+		}
+	}
+	return neTweet, lastTweet, startIdx
 }
